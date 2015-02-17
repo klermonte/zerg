a324b35f60b1f2b4b1d87ac5d28ccc9f92a63f9c
klermonte <klimchek@gmail.com>
2015-02-17 16:56:43 +0300

    Improve ParserTest wrote additional assertions

--- tests/Zerg/ParserTest.php
+++ tests/Zerg/ParserTest.php
@@ -11,12 +11,13 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $schemaArr = [
             'a' => ['int', 8],
             'b' => ['string', 2],
-            'c' => ['enum', 2, [
+            'c' => ['enum', 8, [
                     'values' => [
                         0 => 'A',
                         1 => 'B',
                         2 => 'C',
-                        3 => 'D'
+                        3 => 'D',
+                        97 => 'right'
                     ]
                 ]
             ],
@@ -30,7 +31,15 @@ class ParserTest extends \PHPUnit_Framework_TestCase
             'f' => ['conditional', '/e/e.2', [
                     'schemas' => [
                         'integ' => ['int', 16],
-                        'strin' => ['string', 2]
+                        'strin' => ['string', 2],
+                        'ytjss' => ['enum', 8, [
+                                'values' => [
+                                    3 => 'wrong',
+                                    4 => 'wrong'
+                                ],
+                                'default' => 'right'
+                            ]
+                        ],
                     ],
                     'default' => ['string', 5]
                 ]
@@ -42,6 +51,17 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $result = $parser->parse($schemaObj);
 
         $this->assertInstanceOf('\\Zerg\\DataSet', $result);
+
+        $result = $result->getData();
+
+        $this->assertSame(49, $result['a']);
+        $this->assertSame('23', $result['b']);
+        $this->assertSame('right', $result['c']);
+        $this->assertSame('bc', $result['d']);
+        $this->assertArrayHasKey('e.1', $result['e']);
+        $this->assertSame(113, $result['e']['e.1']);
+        $this->assertSame('ytjss', $result['e']['e.2']);
+        $this->assertSame('right', $result['f']);
 
     }
 
