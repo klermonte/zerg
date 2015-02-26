19b48451fcad344a886816da358cab4715b7b3c4
klermonte <klimchek@gmail.com>
2015-02-26 20:15:26 +0300

    Document String field

--- src/Zerg/Field/String.php
+++ src/Zerg/Field/String.php
@@ -4,10 +4,25 @@ namespace Zerg\Field;
 
 use Zerg\Stream\AbstractStream;
 
+/**
+ * String represents string type data.
+ *
+ * Data, parsed by this type of field returns as it is in binary file.
+ * @since 0.1
+ * */
 class String extends Scalar
 {
+    /**
+     * @var bool Whether returned string should process like UTF-8 encoded.
+     * */
     protected $utf;
 
+    /**
+     * Read string from stream as it is.
+     *
+     * @param AbstractStream $stream Stream from which read.
+     * @return string Returned string.
+     */
     public function read(AbstractStream $stream)
     {
         $string = $stream->getReader()->readString($this->getSize());
