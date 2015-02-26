64f7b0f19f986931afa69123df5070ec7431b58f
klermonte <klimchek@gmail.com>
2015-02-27 00:20:50 +0400

    Document exceptions

--- src/Zerg/Field/ConfigurationException.php
+++ src/Zerg/Field/ConfigurationException.php
@@ -2,6 +2,10 @@
 
 namespace Zerg\Field;
 
+/**
+ * This exception throws when field gets invalid configuration
+ * value and can't cast it to valid.
+ * */
 class ConfigurationException extends \InvalidArgumentException
 {
 }
--- src/Zerg/Field/InvalidKeyException.php
+++ src/Zerg/Field/InvalidKeyException.php
@@ -2,6 +2,11 @@
 
 namespace Zerg\Field;
 
+/**
+ * This exception throws when filed with determined list of values
+ * like Enum {@see Enum} or Conditional {@see Conditional} could not
+ * find key read from Stream and have not default value.
+ * */
 class InvalidKeyException extends \UnexpectedValueException
 {
 }
