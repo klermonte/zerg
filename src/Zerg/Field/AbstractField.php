bb534fb268c2cdd65a131714f464b9395fef471e
klermonte <klimchek@gmail.com>
2015-02-27 20:54:18 +0300

    Document Abstract field

--- src/Zerg/Field/AbstractField.php
+++ src/Zerg/Field/AbstractField.php
@@ -5,34 +5,80 @@ namespace Zerg\Field;
 use Zerg\DataSet;
 use Zerg\Stream\AbstractStream;
 
+/**
+ * This abstract class represents any type of field - an entity that takes, processes and
+ * returns some data from Stream. Any field can be repeated given times. Also field has access
+ * to DataSet from which it takes config values by given paths.
+ *
+ * @since 0.1
+ * @package Zerg\Field
+ */
 abstract class AbstractField
 {
     /**
-     * @var Collection
-     * */
+     * @var Collection The collection field that holds this field.
+     */
     protected $parent = null;
 
     /**
-     * @var DataSet
+     * @var DataSet The DataSet from which this field take values by path.
      */
     protected $dataSet = null;
 
+    /**
+     * @var int Number of times that field should be repeated.
+     */
     protected $count = 1;
+
+    /**
+     * @var callable Callback that changes field repeat count.
+     */
     protected $countCallback;
 
+    /**
+     * Initialize field instance.
+     *
+     * Avery field has one main parameter, which it itself sets by init() {@see init()}
+     * method. Others config values sets smarty by configure() {@see configure()} method.
+     *
+     * @param mixed $mainParam This parameter processed by class implementation.
+     * @param array $properties Array of class properties to be set.
+     */
     public function __construct($mainParam, $properties = [])
     {
+        if (empty($properties)) {
+            $properties = [];
+        }
         $this->init($mainParam);
         $this->configure($properties);
     }
 
+    /**
+     * Implementation classes should override this method to init itself
+     * by given main parameter.
+     *
+     * @param mixed $mainParam This parameter processed by class implementation.
+     */
     abstract public function init($mainParam);
 
+    /**
+     * Read and process data from Stream.
+     *
+     * @api
+     * @param AbstractStream $stream Stream from which field should read.
+     * @return mixed Processed value.
+     */
     abstract public function parse(AbstractStream $stream);
 
-    public function configure($properties = [])
+    /**
+     * Init class properties by given values.
+     *
+     * @param array $properties Array of class properties to be set.
+     * @return static For chaining.
+     */
+    public function configure(array $properties = [])
     {
-        foreach ((array) $properties as $name => $value) {
+        foreach ($properties as $name => $value) {
             $setter = 'set' . $name;
             if (method_exists($this, $setter)) {
                 $this->$setter($value);
@@ -40,10 +86,13 @@ abstract class AbstractField
                 $this->$name = $value;
             }
         }
+        return $this;
     }
 
     /**
-     * @return Collection
+     * Return parent collection.
+     *
+     * @return Collection Collection instance holding this field.
      */
     public function getParent()
     {
@@ -51,8 +100,10 @@ abstract class AbstractField
     }
 
     /**
-     * @param Collection $parent
-     * @return self
+     * Set parent collection.
+     *
+     * @param Collection $parent Collection instance, than will be hold this field.
+     * @return static For chaining.
      */
     public function setParent(Collection $parent)
     {
@@ -61,7 +112,42 @@ abstract class AbstractField
     }
 
     /**
-     * @return DataSet
+     * Return field repeat count.
+     *
+     * @return int Number of times that this field should be repeated.
+     * @throw ConfigurationException If processed value is less zero.
+     */
+    public function getCount()
+    {
+        $this->resolveProperty('count');
+        $count = (int) $this->getCallbackableProperty('count');
+
+        if ($count < 0) {
+            throw new ConfigurationException('Field count should not be less 0');
+        }
+
+        return $count;
+    }
+
+    /**
+     * Set field repeat count.
+     *
+     * Size can be represented as a string containing on of size key words {@see $sizes}.
+     * Also you can set path to already parsed value in DataSet.
+     *
+     * @param mixed $count
+     * @return static For chaining.
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+        return $this;
+    }
+
+    /**
+     * Return DataSet instance from which this field take back linked values.
+     *
+     * @return DataSet DataSet corresponding this field.
      */
     public function getDataSet()
     {
@@ -69,8 +155,10 @@ abstract class AbstractField
     }
 
     /**
-     * @param DataSet $dataSet
-     * @return self
+     * Set to field DataSet instance for back links.
+     *
+     * @param DataSet $dataSet DataSet ro correspond to this field.
+     * @return self For chaining.
      */
     public function setDataSet(DataSet $dataSet)
     {
@@ -78,11 +166,17 @@ abstract class AbstractField
         return $this;
     }
 
-    public function setCount($count)
-    {
-        $this->count = $count;
-    }
-
+    /**
+     * Process, set and return given property.
+     *
+     * Find given property in DataSet if it was set as path string and return it.
+     * Otherwise already set value will be returned.
+     *
+     * @param string $name Property name.
+     * @return mixed Found or already set property value.
+     * @throw ConfigurationException On invalid property value or
+     * if there is not DataSet while property is set as path string.
+     */
     protected function resolveProperty($name)
     {
         $propertyValue = $this->$name;
@@ -105,6 +199,16 @@ abstract class AbstractField
         return $propertyValue;
     }
 
+    /**
+     * Process value by callback.
+     *
+     * Callback should be set by the property of the same name with 'Callback'
+     * suffix on the end ('countCallback' for instance). If callback is not set
+     * property value will be returned.
+     *
+     * @param string $name Property name.
+     * @return mixed Processed or already set property value.
+     */
     protected function getCallbackableProperty($name)
     {
         $callbackName = strtolower($name) . 'Callback';
@@ -113,17 +217,5 @@ abstract class AbstractField
             $value = call_user_func($this->$callbackName, $value);
         }
         return $value;
-    }
-
-    public function getCount()
-    {
-        $this->resolveProperty('count');
-        $count = (int) $this->getCallbackableProperty('count');
-
-        if ($count < 0) {
-            throw new ConfigurationException('Field count should not be less 0');
-        }
-
-        return $count;
     }
 }
