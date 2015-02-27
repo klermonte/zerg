b68b5e3ecf019870ead65aee15633c0346257fcc
klermonte <klimchek@gmail.com>
2015-02-27 21:52:43 +0300

    Fix invalid key exception documentation

--- src/Zerg/Field/InvalidKeyException.php
+++ src/Zerg/Field/InvalidKeyException.php
@@ -5,8 +5,11 @@ namespace Zerg\Field;
 /**
  * This exception throws when filed with determined list of values
  * like Enum {@see Enum} or Conditional {@see Conditional} could not
- * find key read from Stream and have not default value.
- * */
+ * find key read from Stream and has not default value.
+ *
+ * @since 0.1
+ * @package Zerg\Field
+ */
 class InvalidKeyException extends \UnexpectedValueException
 {
 }
