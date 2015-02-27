db0a2f60f669633317a330866f70e3bc23e5e4c6
klermonte <klimchek@gmail.com>
2015-02-27 21:31:35 +0300

    Fix configuration exception documentation

--- src/Zerg/Field/ConfigurationException.php
+++ src/Zerg/Field/ConfigurationException.php
@@ -5,7 +5,10 @@ namespace Zerg\Field;
 /**
  * This exception throws when field gets invalid configuration
  * value and can't cast it to valid.
- * */
+ *
+ * @since 0.1
+ * @package Zerg\Field
+ */
 class ConfigurationException extends \InvalidArgumentException
 {
 }
